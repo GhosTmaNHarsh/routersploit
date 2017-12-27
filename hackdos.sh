7d343cc2e99cf3116d4849a414fbaced6f058ff7
GhosTmaNHarsh <34878554+GhosTmaNHarsh@users.noreply.github.com>
2017-12-27 11:08:18 +0530

    Add files via upload

---
+++ hackdos.sh
@@ -0,0 +1,26 @@
+clear
+echo "   |-------------------------------------|"
+echo "   |      Script by= Harsh Mishra.       |"
+echo "   |                                     |"
+echo "   |   Subscribe to YouTube Channel      |"
+echo "   |           Harsh ki vani             |"
+echo "   |-------------------------------------|"
+echo "   |                                     |"
+echo "   |            Ghostman(+_+)            |"
+echo "   |                                     |"
+echo "   |+++++++++++++++++++++++++++++++++++++|"
+echo "   |           routersploit              |"
+echo "   |+++++++++++++++++++++++++++++++++++++|"
+echo "   |_____________________________________|"
+
+pkg install python2 -y
+git clone https://github.com/reverse-shell/routersploit
+pip2 install -r requirements 
+pip2 install -r requirements-dev.txt
+pip2 install requests 
+echo "_____________________________________________"
+echo ".  |-------------------------------------|"
+echo ".  |   INSTALLATION PROCESS COMPLETED    |"
+echo "   |-------------------------------------|"
+echo "        Thank you for installing....... "
+echo "_____________________________________________"
